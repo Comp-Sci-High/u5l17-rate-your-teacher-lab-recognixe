a803aa52a045fccb15b147cd2fc05e9a9164b197
Mkrah <angel.felix25@compscihigh.org>
2025-04-09 17:00:06 +0000

    what I have sp far

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post("/add/teacher", async (req, res) => {
 
 // Add your SRV string, make sure that the database is called CSHteachers
 async function startServer() {
-  await mongoose.connect("...");
+  await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.uwk2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
   app.listen(3000, () => {
     console.log(`Server running.`);
