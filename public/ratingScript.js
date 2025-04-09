8ffe8583f216a4b9033e1b5b5a88415e92bd372a
Mkrah <angel.felix25@compscihigh.org>
2025-04-09 18:23:30 +0000

    completed

--- public/ratingScript.js
+++ public/ratingScript.js
@@ -1,2 +1,25 @@
 // Add an event listener to the form that upon submits creates a new rating
 // When the rating is created redirect to the ratings page
+ 
+const form = document.querySelector("form")
+
+form.addEventListener("submit", async(e)=>{
+ e.preventDefault()
+ const BookData =new FormData(form)
+ const reqBody= Object.fromEntries(BookData)
+ const response = await fetch("/add",{
+ method: "POST",
+
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify(reqBody)
+ })
+ const data = await response.json()
+ console.log(data)
+ window.location.href ="/"
+
+})
+
+
+
--- public/teacherScript.js
+++ public/teacherScript.js
@@ -1,2 +1,22 @@
 // Add an event listener to the form that upon submits creates a new teacher
 // When the teacher is created redirect to the teachers page
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit", async(e)=>{
+ e.preventDefault()
+ const BookData =new FormData(form)
+ const reqBody= Object.fromEntries(BookData)
+ const response = await fetch("/add",{
+ method: "POST",
+
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify(reqBody)
+ })
+ const data = await response.json()
+ console.log(data)
+ window.location.href ="/"
+
+})
